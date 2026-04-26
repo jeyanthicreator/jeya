06d113494e48a28ea2be48d71e5021c617658b3d
jeyanthicreator <jjeyanthi427@gmail.com>
2026-04-26 17:52:35 +0530

    commit - suacedemo

--- tests/saucefixture.spec.js
+++ tests/saucefixture.spec.js
@@ -1,6 +1,11 @@
 import {test,expect} from "../custom_fixtures/loginfixture"
-test("task1",async({signedInUser})=>{
-    await signedInUser.locator('[class="inventory_item"]',{hasText:prod}).locator('button').click()
+test("task2",async({signedInUser})=>{
+   
+   //await signedInUser.locator('.inventory_item').filter({ hasText: prod }).getByRole('button', { name: 'Add to cart' }).click();
+
+   await signedInUser.locator('[class="inventory_item"]', { hasText:"Backpack" }).locator('button').click();
+   await signedInUser.locator('.shopping_cart_link').click();
+   await expect(signedInUser).toHaveURL("https://www.saucedemo.com/cart.html");
 
     await signedInUser.pause()
 })
